82dbdb3312e01c70875d534a0ec5cd312b9f9c97
Ilya Baykov <i-baykov@internet.ru>
2023-11-13 23:45:13 +0300

    Сделал формы регистрации Входа/Выхода через django.contrib.auth

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import *
 
 urlpatterns = [
-    path('authentication/', authentication, name='authentication'),
+    # path('authentication/', authentication, name='authentication'),
+    path('authentication/', auth_views.LoginView.as_view(), name='authentication'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', dashboard, name='dashboard'),
 ]
--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,23 +2,31 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import *
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-def authentication(request):
-    if request.POST:
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cleaned_data = form.cleaned_data
-            user = authenticate(request, username=cleaned_data['login'], password=cleaned_data['password'])
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse('Вход выполнен успешно')
-                else:
-                    return HttpResponse("Что то не так с аккаунтом")
-            else:
-                return HttpResponse("Неправильный логин")
-    else:
-        form = LoginForm()
-    return render(request, template_name='account/authentication_form.html', context={'form': form})
+@login_required
+def dashboard(request):
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
+
+
+# def authentication(request):
+#     if request.POST:
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             cleaned_data = form.cleaned_data
+#             user = authenticate(request, username=cleaned_data['login'], password=cleaned_data['password'])
+#             if user is not None:
+#                 if user.is_active:
+#                     login(request, user)
+#                     return HttpResponse('Вход выполнен успешно')
+#                 else:
+#                     return HttpResponse("Что то не так с аккаунтом")
+#             else:
+#                 return HttpResponse("Неправильный логин")
+#     else:
+#         form = LoginForm()
+#     return render(request, template_name='account/authentication_form.html', context={'form': form})
