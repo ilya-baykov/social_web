2e1ea3e5c7919dd5fa8b4e5860a3da7a2cfdcb89
Ilya Baykov <i-baykov@internet.ru>
2023-11-16 18:01:52 +0300

    Теперь страницы смены пароля и страницы успешной смены пароля отображаются корректно

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -8,9 +8,7 @@ urlpatterns = [
     path('home', home, name='home'),
     path("login", CustomLoginView.as_view(), name='login'),
     path("logout", auth_views.LogoutView.as_view(), name='logout'),
-    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password-change'),
-    path('password-change/done/',
-         TemplateView.as_view(template_name='registration/password_change_done.html'),
-         name='password_change_done'),
+    path("password_change", auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path("password_change_done", auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path("", dashboard, name='dashboard')
 ]
--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -119,4 +119,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
-LOGOUT_URL = 'logout'
+LOGOUT_REDIRECT_URL = 'login'
