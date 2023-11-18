e5ab409c16460bf6b2825531bcefd7217ac4c6d3
Ilya Baykov <i-baykov@internet.ru>
2023-11-18 14:35:00 +0300

    Изменил текст письма Устранил некоторые опечатки После ввода логина - пароля ( если нет переменной next ) пользователя будет пененапрвлять на страницу дашборда ( по умл)

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -118,7 +118,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = 'login'
 
