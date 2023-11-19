30e29c829e3d6b1675b0068c0ce7ff9340140180
Ilya Baykov <i-baykov@internet.ru>
2023-11-19 16:35:13 +0300

    Сделал страничку пользователя ( Теперь у формы есть ru labels

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -18,4 +18,9 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
+        labels = {
+            'first_name': 'Имя',
+            'last_name': 'Фамилия',
+            'email': 'Электронная почта',
+        }
 
