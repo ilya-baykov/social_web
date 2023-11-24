8a852c87e4f4ae9179321cd3c88167dbc1a68a6e
Ilya Baykov <i-baykov@internet.ru>
2023-11-24 13:42:51 +0300

    Переопределил метод save

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -1,5 +1,8 @@
 from .models import *
 from django import forms
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+import requests
 
 
 class ImagesCreateForm(forms.ModelForm):
@@ -17,4 +20,16 @@ class ImagesCreateForm(forms.ModelForm):
         if extension not in valid_extension:
             raise forms.ValidationError('Полученный url ссылается на неизвестное расширение')
         return url
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+        response = requests.get(image_url)
+        image.image.save(image_name, ContentFile(response.content), save=False)
+        if commit:
+            image.save()
+        return image
 
