4ff71b2e5c634a8d4781f986a112ca1236c31887
Ilya Baykov <i-baykov@internet.ru>
2023-11-24 14:09:04 +0300

    Добавил представление image_create

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import *
+
 
 # Create your views here.
+
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        form = ImagesCreateForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            new_image = form.save(commit=False)
+            new_image.user = request.user
+            new_image.save()
+            messages.success(request, "Изображение успешно добавлено")
+            return redirect(new_image.get_absolute_url())
+    else:
+        form = ImagesCreateForm()
+    return render(request, 'images/image/create.html', {'section': 'images', 'form': form})
