47608fc1b5ae6589d281f30bcc46f1c0d933a409
Ilya Baykov <i-baykov@internet.ru>
2023-11-24 13:08:23 +0300

    Добавил модель Images в админ панель

--- bookmarks/images/admin.py
+++ bookmarks/images/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Images
+
+
 # Register your models here.
+
+@admin.register(Images)
+class ImagesAdmin:
+    list_display = ['title', 'slug', 'image', 'created']
+    list_filter = ['created']
--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -13,6 +13,7 @@ class Images(models.Model):
     image = models.ImageField(upload_to='images/%Y/%m/%d')
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True)
+    user_like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='images_liked', blank=True)
 
     class Meta:
         indexes = [
