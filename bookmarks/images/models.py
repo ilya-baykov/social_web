0325985464560928f595b94837a118d477b30fcd
Ilya Baykov <i-baykov@internet.ru>
2023-11-27 16:01:36 +0300

    Сделал отображение букмарклетов через закладки на сайте

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -7,9 +7,9 @@ from django.utils.text import slugify
 
 class Images(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='images_created', on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100, blank=True)
-    url = models.URLField(max_length=150)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=320, blank=True)
+    url = models.URLField(max_length=320)
     image = models.ImageField(upload_to='images/%Y/%m/%d')
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True)
